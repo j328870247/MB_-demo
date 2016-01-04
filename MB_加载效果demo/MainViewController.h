4b88947c77c7ba786db46b40ffa6646c9d26e423
j328870247 <328870247@qq.com>
2016-01-04 13:51:38 +0800

    ddd

---
+++ MB_加载效果demo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MB_加载效果demo
+//
+//  Created by 谢冬 on 14-11-5.
+//  Copyright (c) 2014年 北京淘伟科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
